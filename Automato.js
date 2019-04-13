02b620180e63a68051b8018e5959fafdf29125fc
Wellington Cesar <wcdiaslima@outlook.com.br>
2019-04-13 15:37:18 -0300

    Fluxo de execução modificado

--- Automato.js
+++ Automato.js
@@ -12,14 +12,21 @@ class Autonomo {
 
 		this.estadoAtual = this.estadoInicial;
 		this.estadosFinais = new Set(arq.estadosFinais);
+
+		if(width > height - 80) this.raio = height / 2 - 80;
+		else this.raio = width / 2 - 25;
 	}
-	reiniciar() {this.estadoAtual = this.estadoInicial;}
+
+	reiniciar() {
+		this.estadoAtual = this.estadoInicial;
+	}
 
 	passo(letra) {
 		let posicao = this.posAlfa(letra);
-		if(posicao >= 0)  this.estadoAtual = this.delta[this.estadoAtual][posicao];
+		if (posicao >= 0) this.estadoAtual = this.delta[this.estadoAtual][posicao];
 		else this.termino(false);
 	}
+
 	posAlfa(letra) {
 		for (let i = 0; i < this.alfabeto.length; i++) {
 			if (this.alfabeto[i] === letra) return i;
@@ -27,18 +34,34 @@ class Autonomo {
 		return -1;
 	}
 	mostrar() {
-		let raio = height / 2 - 80;
 		let x, y;
+
+		strokeWeight(1);
+		noStroke();
+		fill(0);
+		if (estado === 'a') {
+			background(20, 200, 95);
+			text("Aceito!\n( ͡ ͜ʖ ͡ )", width / 2 - 100, height / 2 - 50)
+		} else if (estado === 'r') {
+			background(170, 30, 80);
+			text("Rejeitado!\nヽ( ͡ಠ ʖ̯ ͡ಠ)ﾉ", width / 2 - 150, height / 2 - 50);
+		} else if (estado === 's') {
+			background(170, 30, 80);
+			text("Símbolo não\nreconhecido!\n┐( ͡° ʖ̯ ͡°)┌", width / 2 - 170, height / 2 - 50);
+		}
+		this.ligacoes();
+
 		for (let i = 0; i < this.qtdEstados; i++) {
-			noStroke();
-			x = raio * cos(map(i, 0, this.qtdEstados, 0, TAU)) + width / 2;
-			y = raio * sin(map(i, 0, this.qtdEstados, 0, TAU)) + height / 2 - 50;
+			x = this.raio * cos(map(i, 0, this.qtdEstados, 0, TAU)) + width / 2;
+			y = this.raio * sin(map(i, 0, this.qtdEstados, 0, TAU)) + height / 2 - 50;
 			if (i === this.estadoAtual) fill(255, 22, 84);
 			else fill(243, 255, 189);
+			noStroke();
+
 			circle(x, y, 25);
 			fill(0);
 			text(i, x - 15, y + 2);
-			if(this.estadosFinais.has(i)) {
+			if (this.estadosFinais.has(i)) {
 				stroke(0);
 				noFill();
 				circle(x, y, 30);
@@ -46,82 +69,80 @@ class Autonomo {
 		}
 	}
 	termino(fimcadeia) {
-		window.navigator.vibrate(100);
-		if(estado !== 'r') return;
+		window.navigator.vibrate(400);
+		if (estado !== 'e') return;
 		som.start();
 		som.stop(0.5);
-		if(fimcadeia && this.estadosFinais.has(this.estadoAtual)) {
-			aceito = true;
-			background(20, 200, 95);
-			text("Aceito!\n( ͡ ͜ʖ ͡ )",width / 2 - 100, height / 2 - 50);
+		if (fimcadeia && this.estadosFinais.has(this.estadoAtual)) {
+			estado = 'a';
 			som.freq(500);
-		}
-		else {
-			aceito = false;
-			background(170, 30, 80);
+		} else {
 			som.freq(250);
-			if(fimcadeia) text("Rejeitado!\nヽ( ͡ಠ ʖ̯ ͡ಠ)ﾉ",width / 2 - 150, height / 2 - 50);
-			else text("Símbolo não\nreconhecido!\n┐( ͡° ʖ̯ ͡°)┌",width / 2 - 170, height / 2 - 50);
+			if (fimcadeia) estado = 'r';
+			else estado = 's';
 		}
-		estado = 't';
 	}
-  ligacoes(){
-    let raio = height / 2 - 80, x, y;
-    let coor = [];
-    let alf =[];
-    let i,j;
- 
-    for(i=0;i<this.qtdEstados;i++){
-       alf[i]=[];
-      for (j=0;j<this.qtdEstados;j++){
-        alf[i][j]=[];
-    }}
-    
-    for(i = 0; i < this.qtdEstados; i++) {
-      x = raio * cos(map(i, 0, this.qtdEstados, 0, TAU)) + width / 2;
-      y = raio * sin(map(i, 0, this.qtdEstados, 0, TAU)) + height / 2 - 50;
-      coor[i]=[x,y]; }
-    let k=0;
-    for(i=0;i<this.qtdEstados;i++){
-      for (j=0;j<this.alfabeto.length;j++){
-        alf[i][this.delta[i][j]][alf[i][this.delta[i][j]].length] = this.alfabeto[j];
-    } }
-  
-    for(i=0;i<this.qtdEstados;i++){
-      for(j=0;j<this.alfabeto.length;j++){
-        line(coor[i][k],coor[i][k+1],coor[this.delta[i][j]][k],coor[this.delta[i][j]][k+1])
-        if(i===this.delta[i][j]){
-          let PointM;
-          push();
-          
-          if(coor[i][k+1]<height/2){
-           triangle(coor[i][k], coor[i][k+1]-25, coor[i][k]+10, coor[i][k+1]-35, coor[i][k]-10, coor[i][k+1]-35);
-           PointM = createVector(((coor[i][k]+10)+(coor[i][k]-10))/2+15,((coor[i][k+1]-35)+(coor[i][k+1]-35))/2); 
-          }  
-          else{
-           triangle(coor[i][k], coor[i][k+1]+25, coor[i][k]+10, coor[i][k+1]+35, coor[i][k]-10, coor[i][k+1]+35);
-           PointM = createVector(((coor[i][k]+10)+(coor[i][k]-10))/2,((coor[i][k+1]+35)+(coor[i][k+1]+35))/2);
-          }  
-          
-          push();
-          textSize(20);
-          text(alf[i][this.delta[i][j]],PointM.x,PointM.y+10);
-          pop(); 
-          pop();
-        }
-        else{
-        push();
-        var angulo = atan2(coor[i][k+1]-coor[this.delta[i][j]][k+1],coor[i][k]-coor[this.delta[i][j]][k]);
-        let pM = createVector((coor[i][k]+coor[this.delta[i][j]][k])/2,(coor[i][k+1]+coor[this.delta[i][j]][k+1])/2);
-        translate((pM.x+coor[this.delta[i][j]][k])/2,(pM.y+coor[this.delta[i][j]][k+1])/2);  
-        rotate(angulo-HALF_PI);
-        triangle(-10*0.5, 10, 10*0.5, 10, 0, -10/2);  
-        pop();
-          
-        push();
-        textSize(20);
-        text(alf[i][this.delta[i][j]],(pM.x+coor[this.delta[i][j]][k])/2,(pM.y+coor[this.delta[i][j]][k+1])/2+20);
-        pop(); }
-    } }
-  }
+	ligacoes() {
+		let x, y;
+		let coor = [];
+		let alf = [];
+		let i, j;
+		stroke(0);
+
+		for (i = 0; i < this.qtdEstados; i++) {
+			alf[i] = [];
+			for (j = 0; j < this.qtdEstados; j++) {
+				alf[i][j] = [];
+			}
+		}
+
+		for (i = 0; i < this.qtdEstados; i++) {
+			x = this.raio * cos(map(i, 0, this.qtdEstados, 0, TAU)) + width / 2;
+			y = this.raio * sin(map(i, 0, this.qtdEstados, 0, TAU)) + height / 2 - 50;
+			coor[i] = [x, y];
+		}
+		let k = 0;
+		for (i = 0; i < this.qtdEstados; i++) {
+			for (j = 0; j < this.alfabeto.length; j++) {
+				alf[i][this.delta[i][j]][alf[i][this.delta[i][j]].length] = this.alfabeto[j];
+			}
+		}
+
+		for (i = 0; i < this.qtdEstados; i++) {
+			for (j = 0; j < this.alfabeto.length; j++) {
+				line(coor[i][k], coor[i][k + 1], coor[this.delta[i][j]][k], coor[this.delta[i][j]][k + 1]);
+				if (i === this.delta[i][j]) {
+					let PointM;
+					push();
+
+					if (coor[i][k + 1] < height / 2) {
+						triangle(coor[i][k], coor[i][k + 1] - 25, coor[i][k] + 10, coor[i][k + 1] - 35, coor[i][k] - 10, coor[i][k + 1] - 35);
+						PointM = createVector(((coor[i][k] + 10) + (coor[i][k] - 10)) / 2 + 15, ((coor[i][k + 1] - 35) + (coor[i][k + 1] - 35)) / 2);
+					} else {
+						triangle(coor[i][k], coor[i][k + 1] + 25, coor[i][k] + 10, coor[i][k + 1] + 35, coor[i][k] - 10, coor[i][k + 1] + 35);
+						PointM = createVector(((coor[i][k] + 10) + (coor[i][k] - 10)) / 2, ((coor[i][k + 1] + 35) + (coor[i][k + 1] + 35)) / 2);
+					}
+
+					push();
+					textSize(20);
+					text(alf[i][this.delta[i][j]], PointM.x, PointM.y + 10);
+					pop();
+					pop();
+				} else {
+					push();
+					var angulo = atan2(coor[i][k + 1] - coor[this.delta[i][j]][k + 1], coor[i][k] - coor[this.delta[i][j]][k]);
+					let pM = createVector((coor[i][k] + coor[this.delta[i][j]][k]) / 2, (coor[i][k + 1] + coor[this.delta[i][j]][k + 1]) / 2);
+					translate((pM.x + coor[this.delta[i][j]][k]) / 2, (pM.y + coor[this.delta[i][j]][k + 1]) / 2);
+					rotate(angulo - HALF_PI);
+					triangle(-10 * 0.5, 10, 10 * 0.5, 10, 0, -10 / 2);
+					pop();
+
+					push();
+					textSize(20);
+					text(alf[i][this.delta[i][j]], (pM.x + coor[this.delta[i][j]][k]) / 2, (pM.y + coor[this.delta[i][j]][k + 1]) / 2 + 20);
+					pop();
+				}
+			}
+		}
+	}
 }
--- Main.js
+++ Main.js
@@ -20,55 +20,53 @@ along with AFD. If not, see <https://www.gnu.org/licenses/>.
 let fita, aut, arquivo, som;
 let botao, caixa, entrada;
 let aceito = false;
-let estado = 'p'; //(p)arado, (r)odando e (t)erminado
+let estado = 'p'; //(p)arado, (e)xecutando, (a)ceito, (r)ejeitado e (s)ímbolo desconhecido
 
 function preload() {
-    //Arquivo de autômato disponível para edição em https://www.npoint.io/docs/47de07a0bd5255cdcf78
-    arquivo = loadJSON("https://api.npoint.io/47de07a0bd5255cdcf78");
+	//Arquivo de autômato disponível para edição em https://www.npoint.io/docs/47de07a0bd5255cdcf78
+	arquivo = loadJSON("https://api.npoint.io/47de07a0bd5255cdcf78");
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight-25)
+	createCanvas(windowWidth, windowHeight - 25)
 		.mousePressed(passoManual);
 
-    textFont("Roboto Mono");
-    textAlign(LEFT, CENTER);
-    textSize(50);
+	textFont("Roboto Mono");
+	textAlign(LEFT, CENTER);
+	textSize(50);
 
-    // frameRate(120);//Letras por segundos
-    background(112, 193, 179);
-    fita = new Fita("Não iniciado");
-    aut = new Autonomo(arquivo);
+	//frameRate(2);//Letras por segundos
+	fita = new Fita("001");
+	aut = new Autonomo(arquivo);
 	som = new p5.Oscillator();
 
 	entrada = createInput()
 		.class("um");
 	botao = createButton("Iniciar")
-	    .mousePressed(iniciar)
-	    .class("dois");
+		.mousePressed(iniciar)
+		.class("dois");
 	caixa = createCheckbox("Leitura automática")
 		.class("tres");
 }
 
 function draw() {
-    aut.ligacoes();
-    fita.mostrar();
-    aut.mostrar();
-    if(estado === 'r' && caixa.checked()) {
-	    aut.passo(fita.letra());
-	    fita.passo();
-    }
+	background(112, 193, 179);
+	aut.mostrar();
+	fita.mostrar();
+	if(estado === 'e' && caixa.checked()) {
+		aut.passo(fita.letra());
+		fita.passo();
+	}
 }
 
 function iniciar() {
 	aut.reiniciar();
 	fita.reiniciar(entrada.value());
-	background(112, 193, 179);
-	estado = 'r';
+	estado = 'e';
 }
 
 function passoManual() {
-	if(estado === 'r' && !caixa.checked()) {
+	if (estado === 'e' && !caixa.checked()) {
 		aut.passo(fita.letra());
 		fita.passo();
 	}
