9dc0a4ac5cd028b10c055d2310645a147b1ee8ef
Wellington Cesar <wcdiaslima@outlook.com.br>
2019-04-12 23:00:08 -0300

    Sons, mensagens de aceitação e aviso de símbolo fora do alfabeto

--- Automato.js
+++ Automato.js
@@ -4,44 +4,65 @@ This file is part of AFD.
 */
 
 class Autonomo {
-  constructor(arq) {
-    this.alfabeto = arq.alfabeto;
-    this.qtdEstados = arq.qtdEstados;
-    this.estadoInicial = arq.estadoInicial;
-    this.estadoAtual = this.estadoInicial;
-    this.estadosFinais = new Set(arq.estadosFinais);
-    this.delta = arq.delta;
-  }
-  reiniciar() {this.estadoAtual = this.estadoInicial;}
+	constructor(arq) {
+		this.delta = arq.delta;
+		this.alfabeto = arq.alfabeto;
+		this.qtdEstados = arq.qtdEstados;
+		this.estadoInicial = arq.estadoInicial;
 
-  passo(letra) {
-    this.estadoAtual = this.delta[this.estadoAtual][this.posAlfa(letra)];
-  }
-  posAlfa(letra) {
-    for (let i = 0; i < this.alfabeto.length; i++) {
-      if (this.alfabeto[i] === letra) return i;
-    }
-    return -1;
-  }
-  mostrar() {
-    let raio = height / 2 - 80;
-    let x, y;
-    noStroke();
-    for (let i = 0; i < this.qtdEstados; i++) {
-      x = raio * cos(map(i, 0, this.qtdEstados, 0, TAU)) + width / 2;
-      y = raio * sin(map(i, 0, this.qtdEstados, 0, TAU)) + height / 2 - 50;
-      if (i === this.estadoAtual) fill(255, 22, 84);
-      else fill(243, 255, 189);
-      circle(x, y, 25);
-      fill(0);
-      text(i, x - 15, y + 2);
-    }
-  }
-  termino(fimcadeia) {
-  	window.navigator.vibrate(500);
-  	if(fimcadeia) {
-  		if(this.estadosFinais.has(this.estadoAtual)) alert("Aceito!");
-  		else alert("Rejeitado!!");
-    }
-  }
+		this.estadoAtual = this.estadoInicial;
+		this.estadosFinais = new Set(arq.estadosFinais);
+	}
+	reiniciar() {this.estadoAtual = this.estadoInicial;}
+
+	passo(letra) {
+		let posicao = this.posAlfa(letra);
+		if(posicao >= 0)  this.estadoAtual = this.delta[this.estadoAtual][posicao];
+		else this.termino(false);
+	}
+	posAlfa(letra) {
+		for (let i = 0; i < this.alfabeto.length; i++) {
+			if (this.alfabeto[i] === letra) return i;
+		}
+		return -1;
+	}
+	mostrar() {
+		let raio = height / 2 - 80;
+		let x, y;
+		for (let i = 0; i < this.qtdEstados; i++) {
+			noStroke();
+			x = raio * cos(map(i, 0, this.qtdEstados, 0, TAU)) + width / 2;
+			y = raio * sin(map(i, 0, this.qtdEstados, 0, TAU)) + height / 2 - 50;
+			if (i === this.estadoAtual) fill(255, 22, 84);
+			else fill(243, 255, 189);
+			circle(x, y, 25);
+			fill(0);
+			text(i, x - 15, y + 2);
+			if(this.estadosFinais.has(i)) {
+				stroke(0);
+				noFill();
+				circle(x, y, 30);
+			}
+		}
+	}
+	termino(fimcadeia) {
+		window.navigator.vibrate(100);
+		if(estado !== 'r') return;
+		som.start();
+		som.stop(0.5);
+		if(fimcadeia && this.estadosFinais.has(this.estadoAtual)) {
+			aceito = true;
+			background(20, 200, 95);
+			text("Aceito!\n( ͡ ͜ʖ ͡ )",width / 2 - 100, height / 2 - 50);
+			som.freq(500);
+		}
+		else {
+			aceito = false;
+			background(170, 30, 80);
+			som.freq(250);
+			if(fimcadeia) text("Rejeitado!\nヽ( ͡ಠ ʖ̯ ͡ಠ)ﾉ",width / 2 - 150, height / 2 - 50);
+			else text("Símbolo não\nreconhecido!\n┐( ͡° ʖ̯ ͡°)┌",width / 2 - 170, height / 2 - 50);
+		}
+		estado = 't';
+	}
 }
--- Fita.js
+++ Fita.js
@@ -3,37 +3,36 @@ Copyright 2019 Joyce Emanuele, Wellington Cesar
 This file is part of AFD.
 */
 
-let larg = 30;
+let larg = 30.005;
 
 class Fita {
-  constructor(c) {
-    this.cadeia = c;
-    this.atual = 0;
-  }
-  reiniciar(c) {
-  	if(c) this.cadeia = c;
-  	this.atual = 0;
-  }
+	constructor(c) {
+		this.cadeia = c;
+		this.atual = 0;
+	}
+	reiniciar(c) {
+		this.cadeia = c;
+		this.atual = 0;
+	}
 
-  mostrar() {
-    noStroke();
-    fill(36, 123, 160);
-    rect(0, height - 100, width, 100);
-    fill(0);
-    text(this.cadeia, width / 2 - larg * this.atual - larg * 0.59, height - 50);
-    strokeWeight(3);
-    stroke(255, 22, 84);
-    line(width / 2 - larg / 2 - 3, height -100, width / 2 - larg / 2 - 3, height);
-    line(width / 2 + larg / 2 - 3, height -100, width / 2 + larg / 2 - 3, height);
-  }
-  passo() {
-    this.atual ++;
-    if(this.atual === this.cadeia.length) {
-    	rodando = false;
-    	aut.termino(true);
-    }
-  }
-  letra() {
-    return this.cadeia.charAt(this.atual);
-  }
+	mostrar() {
+		noStroke();
+		fill(36, 123, 160);
+		rect(0, height - 100, width, 100);
+		fill(0);
+		text(this.cadeia, width / 2 - larg * this.atual - larg * 0.59, height - 50);
+		strokeWeight(3);
+		stroke(255, 22, 84);
+		line(width / 2 - larg / 2 - 3, height -100, width / 2 - larg / 2 - 3, height);
+		line(width / 2 + larg / 2 - 3, height -100, width / 2 + larg / 2 - 3, height);
+	}
+	passo() {
+		this.atual ++;
+		if(this.atual === this.cadeia.length) {
+			aut.termino(true);
+		}
+	}
+	letra() {
+		return this.cadeia.charAt(this.atual);
+	}
 }
